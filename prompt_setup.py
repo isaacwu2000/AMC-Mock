e55a6220181befdd6b36dfe2670159c3174c589f
Isaac Wu <isaacwu2000@gmail.com>
2025-08-08 12:36:41 +0000

    fixed prompt method to ensure word problems are generated

--- prompt_setup.py
+++ prompt_setup.py
@@ -3,29 +3,29 @@ def get_prompts(question_number, example_problems, previous_problems = []):
     # and is inconsistent in solving problems with them
     concept_distribution = {
         1: "Algebra",
-        2: "Algebra",
+        2: "Algebra, word problem",
         3: "Number Theory",
-        4: "Number Theory",
+        4: "Counting and Probability, word problem",
         5: "Number Theory",
-        6: "Counting and Probability",
+        6: "Other (Not algebra, number theory, geometry, or counting and probability), word problem",
         7: "Number Theory",
         8: "Counting and Probability",
         9: "Counting and Probability",
-        10: "Number Theory",
+        10: "Number Theory, word problem",
         11: "Number Theory",
-        12: "Algebra",
+        12: "Algebram word problem",
         13: "Algebra",
-        14: "Counting and Probability",
-        15: "Number Theory",
+        14: "Counting and Probability, word problem",
+        15: "Number Theory, word problem",
         16: "Algebra",
         17: "Counting and Probability",
-        18: "Number Theory",
+        18: "Number Theory, word problem",
         19: "Algebra",
         20: "Counting and Probability",
         21: "Algebra",
-        22: "Algebra",
+        22: "Algebra, word problem",
         23: "Algebra",
-        24: "Counting and Probability",
+        24: "Counting and Probability, word problem",
         25: "Algebra"
     }
     
@@ -41,6 +41,6 @@ def get_prompts(question_number, example_problems, previous_problems = []):
     for example_problem in example_problems[question_number]:
         system_prompt += "\n" + str(example_problem)
 
-    user_prompt = f"Create a problem {question_number} for the AMC10 (2026) on the topic of {concept_distribution[question_number]}."
+    user_prompt = f"Create a problem {question_number} for the AMC10 (2050) on the topic of {concept_distribution[question_number]}."
     
     return {"system":system_prompt, "user":user_prompt, "topic":{concept_distribution[question_number]}}
